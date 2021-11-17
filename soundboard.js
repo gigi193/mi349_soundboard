8dfa3138f91810d5bf38c4243ff4b8c32525a5d5
GG <gerganayordanova9@gmail.com>
2021-11-16 19:23:42 -0500

    soundboard

--- soundboard.js
+++ soundboard.js
@@ -1,38 +1,10 @@
-var ButtonApplause = document.getElementById('B_applause');
-var ButtonBubbles = document.getElementById('B_bubbles');
-var ButtonCamera = document.getElementById('B_camera');
-
-ButtonApplause.addEventListener("click",function(){
-    document.getElementById("applause").play()
-})
-
-ButtonApplause.addEventListener("mouseenter",function(){
-    document.getElementById("applause").play()
-})
-
-ButtonBubbles.addEventListener("click",function(){
-    document.getElementById("bubbles").play()
-})
-
-ButtonBubbles.addEventListener("mouseenter",function(){
-    document.getElementById("bubbles").play()
-})
-
-ButtonCamera.addEventListener("click",function(){
-    document.getElementById("camera").play()
-})
-
-ButtonCamera.addEventListener("mouseenter",function(){
-    document.getElementById("camera").play()
-})
-
-function PlaySound1(){
+function playSound() {
     var sound = new Audio("applause_y.wav");
     sound.play();
-}
+};
 
 function PlaySound2(){
-    var sound = new Audio("bubbles_stx.wav");
+    var sound = new Audio("bubbles_sfx.wav");
     sound.play();
 }
 
